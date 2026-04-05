5ab10a4239ca78e0a26e8a9a599ceae59dfc6ba5
ScannerBot <anshhans13@gmail.com>
2026-04-05 13:13:59 -0500

    Revert "Speed up initial boot loading"
    This reverts commit 64735ad639cbb1fa761c9631311c6e3d886aa0ee.

--- src/components/layout/AppShell.jsx
+++ src/components/layout/AppShell.jsx
@@ -138,7 +138,7 @@ function DesktopSidebar({ pathname }) {
 }
 
 export default function AppShell() {
-  const { authReady, bootProgress, bootStatus, hasBootCache, loading } = useMarketplace();
+  const { authReady, bootProgress, bootStatus, loading } = useMarketplace();
   const location = useLocation();
   const [isDesktop, setIsDesktop] = useState(() =>
     typeof window === "undefined" ? false : window.matchMedia("(min-width: 1024px)").matches,
@@ -166,17 +166,16 @@ export default function AppShell() {
       return undefined;
     }
 
-    const canRevealFromCache = hasBootCache && !loading;
-    if (loading || (!authReady && !canRevealFromCache)) {
+    if (!authReady || loading) {
       return undefined;
     }
 
     const timeoutId = window.setTimeout(() => {
       setBootReady(true);
-    }, canRevealFromCache ? 50 : isDesktop ? 120 : 90);
+    }, isDesktop ? 120 : 90);
 
     return () => window.clearTimeout(timeoutId);
-  }, [authReady, bootReady, hasBootCache, isDesktop, loading]);
+  }, [authReady, bootReady, isDesktop, loading]);
 
   if (!bootReady) {
     return <AppLaunchScreen compact={isDesktop} progress={bootProgress} status={bootStatus} />;
